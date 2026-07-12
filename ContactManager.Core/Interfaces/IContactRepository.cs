90ab6561939ef4224bd674c9fc6a67407c9d349a
mariissss <maria11.ukr@gmail.com>
2026-07-12 18:14:22 +0300

    Added core layer: contact entity and IContactRepository interface

---
+++ ContactManager.Core/Interfaces/IContactRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContactManager.Core.Entities;
+
+namespace ContactManager.Core.Interfaces
+{
+    public interface IContactRepository
+    {
+        Task<IEnumerable<Contact>> GetContactsAsync(string? searchTerm, string? sortBy, bool isAscending, int pageNumber, int pageSize);
+
+        Task<int> GetTotalCountAsync(string? searchTerm);
+
+        Task<Contact?> GetByIdAsync(int id);
+
+        // велике додавання з CSV 
+        Task<int> AddRangeAsync(IEnumerable<Contact> contacts);
+
+        Task<bool> UpdateAsync(Contact contact);
+
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ ContactManager.Infrastructure/SqlScripts/CreateDB.sql
@@ -0,0 +1,21 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'ContactManagerDB')
+BEGIN
+    CREATE DATABASE ContactManagerDB;
+END
+GO
+
+USE ContactManagerDB;
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Contacts]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE [dbo].[Contacts] (
+        [Id] INT IDENTITY(1,1) PRIMARY KEY,
+        [Name] NVARCHAR(100) NOT NULL,
+        [DateOfBirth] DATE NOT NULL,
+        [Married] BIT NOT NULL,
+        [Phone] NVARCHAR(50) NOT NULL,
+        [Salary] DECIMAL(18, 2) NOT NULL
+    );
+END
+GO
